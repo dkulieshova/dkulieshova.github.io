b2c7983d78385dbfcb23f302b1ec9fdf17303850
daria <54882674+dkulieshova@users.noreply.github.com>
2024-06-13 14:57:15 +0200

    Style v0.2

--- script.js
+++ script.js
@@ -10,6 +10,29 @@ async function changeLanguage(lang) {
 
     document.getElementById('title').innerText = content.title;
     document.getElementById('description').innerText = content.description;
+    document.getElementById('name').innerText = content.name;
+    document.getElementById('cours').innerText = content.cours;
+    document.getElementById('mail').innerText = content.mail;
+    document.getElementById('education-title').innerText = content.education;
+    document.getElementById('languages-title').innerText = content.languages;
+    document.getElementById('video-title').innerText = content.video;
+
+    const tableHeaders = document.querySelectorAll('th');
+    const tableRows = document.querySelectorAll('td');
+    const headers = content.table.header;
+    const rows = content.table.rows;
+
+    tableHeaders[0].textContent = headers.establishment;
+    tableHeaders[1].textContent = headers.details;
+    tableHeaders[2].textContent = headers.country;
+    tableHeaders[3].textContent = headers.years;
+
+    for (let i = 0; i < rows.length; i++) {
+        tableRows[i * 4].textContent = rows[i].establishment;
+        tableRows[i * 4 + 1].textContent = rows[i].details;
+        tableRows[i * 4 + 2].textContent = rows[i].country;
+        tableRows[i * 4 + 3].textContent = rows[i].years;
+    }
 
     // Update the lang attribute of the HTML tag
     document.documentElement.lang = lang;
